d1a45ddb2f91d8137d10a0ad81e40a938e1651d7
colin <colin.urban@gmail.com>
2013-03-01 05:21:54 -0500

    Added expaned view for more in depth note editing

--- js/note.js
+++ js/note.js
@@ -7,7 +7,7 @@
         input();
         }
         });
-        $('#input').focus();
+       
         //function to load an display notes
         function load(){
         var notes = $.getJSON('notes.json');
@@ -19,7 +19,7 @@
             //display notes
                 for (var i = 0; i < length; i++) {
                     if (typeof data.posts[i]['title'] != 'undefined'){
-                    $('#notes').append('<div class="one-third column note"><span style="width:100%;background:#ddd;padding:2px;padding-right:10px; padding-left:10px;"><b>'+data.posts[i]['title'].replace("@","")+'</b><a class="delete" onclick=del(\''+data.posts[i]['title']+'\')>X</a></span><div id="'+i+'"  onClick="this.contentEditable=\'true\';" onblur="e('+i+')" class="inner">'+data.posts[i]['text']+'</div></div>');  
+                    $('#notes').append('<div class="one-third column note"><span style="width:100%;background:#ddd;padding:2px;padding-right:10px; padding-left:10px;"><b>'+data.posts[i]['title'].replace("@","")+'</b><a class="delete" onclick=del(\''+data.posts[i]['title']+'\')>X</a><a class="expand" onclick="zoom('+i+')">+</a></span><div id="'+i+'"  onClick="this.contentEditable=\'true\';" onblur="e('+i+')" class="inner">'+data.posts[i]['text']+'</div></div>');  
                     }
                 };
             }); 
@@ -47,6 +47,32 @@
                 title = data.posts[id]['title'];
                 to_json(title,text)
             });
+        }
+
+        function zoom(id){
+            console.log(id);
+             var div = '#'+id;
+        var text = $(div).html();
+            $.getJSON('notes.json', function(data) {
+                data.posts.reverse();
+                title = data.posts[id]['title'];
+                $('#pop_title').html('<span style="margin-left:20px; font-size:18px">'+title.replace("@","")+'</span><a style="margin-right:10px;" class="delete" onclick="zoom_c(\''+title+'\')">X</a>');
+                $('#pop_inner').html(text);
+                $('#pop').slideDown();
+            });
+
+           
+            
+        }
+
+        function zoom_c(title){
+            
+            text =  $('#pop_inner').html();
+
+            to_json(title,text);
+             $('#pop').slideUp();
+
+
         }
         //main function to filter and sort input
         function input(){
